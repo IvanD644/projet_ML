869c82051a08fbe582bd8ce6d922d5c478651abc
Ivan <ivan.dylevskiy@gmail.com>
2024-04-12 17:06:14 +0200

    Implement linear_regression.py

--- 363759_361543_355522_project/src/methods/linear_regression.py
+++ 363759_361543_355522_project/src/methods/linear_regression.py
@@ -14,38 +14,47 @@ class LinearRegression(object):
             and call set_arguments function of this class.
         """
         self.lmda = lmda
+        self.N = None
+        self.D = None # D already counts the bias term
+        self.C = None
+        self.weights = None
 
     def fit(self, training_data, training_labels):
         """
             Trains the model, returns predicted labels for training data.
             Arguments:
                 training_data (np.array): training data of shape (N,D)
-                training_labels (np.array): regression target of shape (N,regression_target_size)
+                training_labels (np.array): regression target of shape (N,C)
             Returns:
-                pred_labels (np.array): target of shape (N,regression_target_size)
+                pred_labels (np.array): target of shape (N,C)
         """
         ##
         ###
         #### YOUR CODE HERE!
         ###
         ##
+        self.N = training_data.shape[0]
+        self.D = training_data[0].shape[0]
+        self.C = training_labels[0].shape[0]
+        I = np.eye(self.D)
+        inverse = np.linalg.inv(training_data.T @ training_data + I * self.lmda)
+        self.weights = inverse @ training_data.T @ training_labels
+        
+        return self.predict(training_data)
 
-        return pred_regression_targets
 
-
-def predict(self, test_data):
+    def predict(self, test_data):
         """
             Runs prediction on the test data.
             
             Arguments:
                 test_data (np.array): test data of shape (N,D)
             Returns:
-                test_labels (np.array): labels of shape (N,regression_target_size)
+                test_labels (np.array): labels of shape (N,C)
         """
         ##
         ###
         #### YOUR CODE HERE!
         ###
         ##
-
-        return pred_regression_targets
+        return test_data @ self.weights
